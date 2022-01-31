515bc33f4b92bad94eaa0d00742e4e5c7b83f13d
Maks Nabokov <max.nab@eosdt.com>
2022-01-31 16:43:46 +0300

    build fix

--- pallets/equilibrium-curve-amm/src/lib.rs
+++ pallets/equilibrium-curve-amm/src/lib.rs
@@ -125,6 +125,7 @@ pub mod pallet {
 
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
+	#[pallet::without_storage_info]
     pub struct Pallet<T>(_);
 
     /// Current number of pools (also ID for the next created pool)
